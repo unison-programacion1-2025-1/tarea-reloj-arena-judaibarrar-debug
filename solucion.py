e0f6757511622d2d48a25d858367bf6a4c07c873
judaibarrar-debug <judaibarrar@gmail.com>
2025-11-28 21:15:29 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -3,7 +3,7 @@ def reloj_arena(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
     if m <= 0:
         print("Error: La altura debe ser un entero positivo")
-        sys.exit(1)  # termina el programa inmediatamente
+        
 
     
     
