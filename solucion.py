5a86f854fa4da4cef6aa5fd89ba7096e54b76931
judaibarrar-debug <judaibarrar@gmail.com>
2025-11-28 21:24:05 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -2,8 +2,10 @@
 def reloj_arena(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
     if m <= 0:
-        print("Error: La altura debe ser un entero positivo")
-        
+        print( "Error: El símbolo debe ser un único carácter")
+
+    if s != 1:
+        print( "Error: El símbolo debe ser un único carácter")
 
     
     
